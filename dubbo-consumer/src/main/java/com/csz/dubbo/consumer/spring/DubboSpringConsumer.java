ef67a7a79795b968f6df1fcb3a85a76407d6d102
mpif <caiszf@163.com>
2019-04-09 10:30:42 +0800

    dubbo直连模式

--- dubbo-consumer/src/main/java/com/csz/dubbo/consumer/spring/DubboSpringConsumer.java
+++ dubbo-consumer/src/main/java/com/csz/dubbo/consumer/spring/DubboSpringConsumer.java
@@ -16,8 +16,14 @@ public class DubboSpringConsumer {
         //Prevent to get IPV6 address,this way only work in debug mode
         //But you can pass use -Djava.net.preferIPv4Stack=true,then it work well whether in debug mode or not
         System.setProperty("java.net.preferIPv4Stack", "true");
+
+        String[] xmls = new String[]{
+//            "dubbo-practise-consumer.xml"
+                "dubbo-consumer-noRegistry.xml"
+        };
+
 //        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-consumer.xml"});
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"dubbo-practise-consumer.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(xmls);
         context.start();
         DubboServiceRpcApi demoService = (DubboServiceRpcApi) context.getBean("dubboServiceRpcApi"); // get remote service proxy
 
--- dubbo-provider/src/main/java/com/csz/dubbo/provider/spring/DubboSpringProvider.java
+++ dubbo-provider/src/main/java/com/csz/dubbo/provider/spring/DubboSpringProvider.java
@@ -14,8 +14,14 @@ public class DubboSpringProvider {
         //Prevent to get IPV6 address,this way only work in debug mode
         //But you can pass use -Djava.net.preferIPv4Stack=true,then it work well whether in debug mode or not
         System.setProperty("java.net.preferIPv4Stack", "true");
+
+        String[] xmls = new String[]{
+//            "dubbo-practise-provider.xml"
+            "dubbo-provider-noRegistry.xml"
+        };
+
 //        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-provider.xml"});
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"dubbo-practise-provider.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(xmls);
         context.start();
 
         System.in.read(); // press any key to exit
